36dc964c18b935144ef2b952ef4bdfa1be08325e
Mehmet Eren Ergin <merenergin35@gmail.com>
2022-05-30 23:44:43 +0300

    commit1144

--- src/test/java/practice/Q05.java
+++ src/test/java/practice/Q05.java
@@ -1,20 +1,31 @@
 package practice;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class Q05 {
-         /*
-       // ...Exercise5...
-       // Navigate to website  https://testpages.herokuapp.com/styled/index.html
-       // Under the  ORIGINAL CONTENTS
-       // click on Alerts
-       // print the URL
-       // navigate back
-       // print the URL
-       // Click on Basic Ajax
-       // print the URL
-       // enter value-> 20 and Enter
-       // and then verify Submitted Values is displayed open page
-       // close driver
-        */
+
+       public static void main(String[] args) {
+           // ...Exercise5...
+           System.setProperty("webdriver.chrome.driver","src/resources/drivers/chromedriver.exe");
+           WebDriver driver= new ChromeDriver();
+           driver.manage().window().maximize();
+           driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+           // Navigate to website  https://testpages.herokuapp.com/styled/index.html
+           driver.get("https://testpages.herokuapp.com/styled/index.html");
+           // Under the  ORIGINAL CONTENTS
+           // click on Alerts
+           // print the URL
+           // navigate back
+           // print the URL
+           // Click on Basic Ajax
+           // print the URL
+           // enter value-> 20 and Enter
+           // and then verify Submitted Values is displayed open page
+           // close driver
+       }
 
 
 }
